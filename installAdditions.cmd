7fd5af22c3d404e79703fa9f1314f5db12397153
Gian - Luca Veronese <67872150+vaxx-y@users.noreply.github.com>
2021-03-14 01:02:00 +0100

    update directory

--- installAdditions.cmd
+++ installAdditions.cmd
@@ -11,40 +11,42 @@ echo        INSTALLING
 echo.
 echo ========================
 echo.
-copy %~dp0\adds\LeftButton.png %~dp0\content\textures\ui\PurchasePrompt\LeftButton.png
+cd %~dp0
+cd ..
+copy %~dp0\adds\LeftButton.png %cd%\content\textures\ui\PurchasePrompt\LeftButton.png
 echo COPIED FILE LeftButton.png
-copy %~dp0\adds\LeftButtonDown.png %~dp0\content\textures\ui\PurchasePrompt\LeftButtonDown.png
+copy %~dp0\adds\LeftButtonDown.png %cd%\content\textures\ui\PurchasePrompt\LeftButtonDown.png
 echo COPIED FILE LeftButtonDown.png
-copy %~dp0\adds\LoadingBG.png %~dp0\content\textures\ui\PurchasePrompt\LoadingBG.png
+copy %~dp0\adds\LoadingBG.png %cd%\content\textures\ui\PurchasePrompt\LoadingBG.png
 echo COPIED FILE LoadingBG.png
-copy %~dp0\adds\PurchasePromptBG.png %~dp0\content\textures\ui\PurchasePrompt\PurchasePromptBG.png
+copy %~dp0\adds\PurchasePromptBG.png %cd%\content\textures\ui\PurchasePrompt\PurchasePromptBG.png
 echo COPIED FILE PurchasePromptBG.png
-copy %~dp0\adds\RightButton.png %~dp0\content\textures\ui\PurchasePrompt\RightButton.png
+copy %~dp0\adds\RightButton.png %cd%\content\textures\ui\PurchasePrompt\RightButton.png
 echo COPIED FILE RightButton.png
-copy %~dp0\adds\RightButtonDown.png %~dp0\content\textures\ui\PurchasePrompt\RightButtonDown.png
+copy %~dp0\adds\RightButtonDown.png %cd%\content\textures\ui\PurchasePrompt\RightButtonDown.png
 echo COPIED FILE RightButtonDown.png
-copy %~dp0\adds\SingleButton.png %~dp0\content\textures\ui\PurchasePrompt\SingleButton.png
+copy %~dp0\adds\SingleButton.png %cd%\content\textures\ui\PurchasePrompt\SingleButton.png
 echo COPIED FILE SingleButton.png
-copy %~dp0\adds\SingleButtonDown.png %~dp0\content\textures\ui\PurchasePrompt\SingleButtonDown.png
+copy %~dp0\adds\SingleButtonDown.png %cd%\content\textures\ui\PurchasePrompt\SingleButtonDown.png
 echo COPIED FILE SingleButtonDown.png
 echo.
 echo  INSTALLED PURCHASEPROMPT
 echo.
-copy %~dp0\adds\dialog_white.png %~dp0\content\textures\ui\dialog_white.png
+copy %~dp0\adds\dialog_white.png %cd%\content\textures\ui\dialog_white.png
 echo COPIED FILE dialog_white.png
-copy %~dp0\adds\MenuBackground.png %~dp0\content\textures\ui\Settings\MenuBarAssets\MenuBackground.png
+copy %~dp0\adds\MenuBackground.png %cd%\content\textures\ui\Settings\MenuBarAssets\MenuBackground.png
 echo COPIED FILE MenuBackground.png
-copy %~dp0\adds\MenuButton.png %~dp0\content\textures\ui\Settings\MenuBarAssets\MenuButton.png
+copy %~dp0\adds\MenuButton.png %cd%\content\textures\ui\Settings\MenuBarAssets\MenuButton.png
 echo COPIED FILE MenuButton.png
-copy %~dp0\adds\MenuButtonSelected.png %~dp0\content\textures\ui\Settings\MenuBarAssets\MenuButtonSelected.png
+copy %~dp0\adds\MenuButtonSelected.png %cd%\content\textures\ui\Settings\MenuBarAssets\MenuButtonSelected.png
 echo COPIED FILE MenuButtonSelected.png
-copy %~dp0\adds\MenuSelection.png %~dp0\content\textures\ui\Settings\MenuBarAssets\MenuSelection.png
+copy %~dp0\adds\MenuSelection.png %cd%\content\textures\ui\Settings\MenuBarAssets\MenuSelection.png
 echo COPIED FILE MenuSelection.png
-copy %~dp0\adds\EscapeIcon.png %~dp0\content\textures\ui\Settings\Help\EscapeIcon.png
+copy %~dp0\adds\EscapeIcon.png %cd%\content\textures\ui\Settings\Help\EscapeIcon.png
 echo COPIED FILE EscapeIcon.png
-copy %~dp0\adds\LeaveIcon.png %~dp0\content\textures\ui\Settings\Help\LeaveIcon.png
+copy %~dp0\adds\LeaveIcon.png %cd%\content\textures\ui\Settings\Help\LeaveIcon.png
 echo COPIED FILE LeaveIcon.png
-copy %~dp0\adds\ResetIcon.png %~dp0\content\textures\ui\Settings\Help\ResetIcon.png
+copy %~dp0\adds\ResetIcon.png %cd%\content\textures\ui\Settings\Help\ResetIcon.png
 echo COPIED FILE ResetIcon.png
 echo.
 echo  INSTALLED SETTINGS MODAL
